454231bffc7e445f07198b8dada06bdbd21f8818
Gabriela <gabrielaehrenbrink@gmail.com>
2024-02-13 14:22:48 +0000

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Gabriela Ehrenbrink on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
